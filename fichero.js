751eee44bd35b084d5b557518ed53855fd100fa4
gamingjose <josejaviermajim@gmail.com>
2020-01-31 21:50:28 +0100

    Menu con JavaScript y arreglados errores en Index.html

---
+++ fichero.js
@@ -0,0 +1,16 @@
+/*function myFunction() {
+	if (document.getElementById("contenidoMenu").hidden==false){	
+		document.getElementById("contenidoMenu").hidden=true;		
+	}
+	else {document.getElementById("contenidoMenu").hidden=false;
+			}}*/
+			
+			function myFunction() {
+	if (document.getElementById("contenidoMenu").style.opacity=="1"){	
+		document.getElementById("contenidoMenu").style.opacity="0";			
+	}
+	else {document.getElementById("contenidoMenu").style.opacity="1";	
+
+			}}
+			
+			
